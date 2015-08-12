811cb024598e73f3a9279534fe87639294387051
Jeff Tsai <jefftsai8049@gmail.com>
2015-08-12 10:42:25 +0800

    awesom program fucking cool

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,17 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QString str = "hello";
+    qDebug() << str;
+    str.prepend("haha");
+    qDebug() << str;
+    str.append("yaya");
+    qDebug() << str;
+
+    QString str2 = "momo ";
+    QString str3 = str2+str;
+    qDebug() << str3.size() << str3;
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
+#include <QString>
 
 namespace Ui {
 class MainWindow;
